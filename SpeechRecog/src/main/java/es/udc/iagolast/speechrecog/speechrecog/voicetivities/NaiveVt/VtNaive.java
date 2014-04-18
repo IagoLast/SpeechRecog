05c18bdbceabff27ccefd59569b64ca3fac4b048
iagolast <iago.lastra@gmail.com>
2014-04-18 20:39:58 +0200

    Added app icon Changed app name Added app style Updated naive Voicetivity

--- SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/voicetivities/NaiveVt/VtNaive.java
+++ SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/voicetivities/NaiveVt/VtNaive.java
@@ -3,8 +3,11 @@ package es.udc.iagolast.speechrecog.speechrecog.voicetivities.NaiveVt;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.AlarmClock;
 import android.provider.MediaStore;
 import android.util.Log;
+
+import java.util.Calendar;
 
 import es.udc.iagolast.speechrecog.speechrecog.voicetivities.Voicetivity;
 
@@ -28,13 +31,24 @@ public class VtNaive implements Voicetivity {
             Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             service.startActivity(intent);
-        } else if (speech.matches("buscar .*")) {
+        } else if (speech.matches("buscar .*") || speech.matches("busca .*")) {
             String query = speech.substring(speech.indexOf(" "));
             Log.d("Buscando:", query);
             Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.putExtra(SearchManager.QUERY, query);
             service.startActivity(intent);
+        } else if (speech.matches("avísame en .* minutos")) {
+            String minutesString = speech.substring(speech.indexOf("en ") + 3, speech.indexOf("minutos") - 1);
+            Log.d("Recordatorio:", minutesString);
+            int minutes = Integer.valueOf(minutesString);
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                    .putExtra(AlarmClock.EXTRA_MESSAGE, "Aviso")
+                    .putExtra(AlarmClock.EXTRA_HOUR, Calendar.getInstance().getTime().getHours())
+                    .putExtra(AlarmClock.EXTRA_MINUTES, Calendar.getInstance().getTime().getMinutes() + minutes);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            service.startActivity(intent);
+
         }
     }
 }
