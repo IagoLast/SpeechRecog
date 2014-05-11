8a4828bb74676da83367b571dc37fe476546434c
kenkeiras <me@codigoparallevar.com>
2014-05-11 20:40:40 +0200

    Check IMAP user/password vality before saving it

--- SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/SettingsActivity.java
+++ SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/SettingsActivity.java
@@ -2,15 +2,22 @@ package es.udc.iagolast.speechrecog.speechrecog;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import es.udc.iagolast.speechrecog.speechrecog.mailClient.imap.IMAPMailClient;
 
 public class SettingsActivity extends Activity {
     SharedPreferences sharedPreferences;
     EditText editTextMail;
     EditText editTextPassword;
+    Button butSavePrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,6 @@ public class SettingsActivity extends Activity {
 
     @Override
     protected void onStop() {
-        savePreferences();
         super.onStop();
     }
 
@@ -29,6 +35,7 @@ public class SettingsActivity extends Activity {
         sharedPreferences = getSharedPreferences("ziriPrefs", MODE_PRIVATE);
         editTextMail = (EditText) findViewById(R.id.editText);
         editTextPassword = (EditText) findViewById(R.id.editText2);
+        butSavePrefs = (Button) findViewById(R.id.but_save_prefs);
 
         populateUI();
     }
@@ -50,9 +57,36 @@ public class SettingsActivity extends Activity {
     }
 
 
-    private void savePreferences() {
-        String mail = editTextMail.getText().toString();
-        String password = editTextPassword.getText().toString();
+    public void checkPreferences(View v){
+        final String username = editTextMail.getText().toString();
+        final String password = editTextPassword.getText().toString();
+
+        butSavePrefs.setText(R.string.checking_credentials);
+        new AsyncTask<Void, Void, Boolean>(){
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                return IMAPMailClient.checkCredentials(username, password,
+                                                       "imap.gmail.com", 993);
+            }
+
+            @Override
+            protected void onPostExecute(Boolean validCredentials){
+                butSavePrefs.setText(getString(R.string.save_preferences));
+                if (validCredentials == null) {
+                    Log.e("SpeechRecog/SettingsActivity", "NULL can't be returned here!");
+                } else if (validCredentials){
+                    savePreferences(username, password);
+                } else {
+                    Toast.makeText(SettingsActivity.this, getString(R.string.invalid_user_pass),
+                                   Toast.LENGTH_SHORT).show();
+                }
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+    }
+
+
+    private void savePreferences(String mail, String password) {
 
         if (!TextUtils.isEmpty(mail) && !TextUtils.isEmpty(password)) {
             sharedPreferences.edit().putString("mail", mail).commit();
