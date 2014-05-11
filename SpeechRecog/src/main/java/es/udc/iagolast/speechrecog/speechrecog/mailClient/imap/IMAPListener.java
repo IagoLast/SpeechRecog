c9a24e195ceb9477549ba35a77d50222549b8107
kenkeiras <me@codigoparallevar.com>
2014-05-11 21:35:41 +0200

    IMAP listener sliced to functions

--- SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/mailClient/imap/IMAPListener.java
+++ SpeechRecog/src/main/java/es/udc/iagolast/speechrecog/speechrecog/mailClient/imap/IMAPListener.java
@@ -28,68 +28,114 @@ class IMAPListener implements ProtocolCommandListener {
     public void protocolCommandSent(ProtocolCommandEvent protocolCommandEvent) {
     }
 
-    @Override
-    public void protocolReplyReceived(ProtocolCommandEvent event) {
-        String message = event.getMessage();
-        Log.d("IMAPListener/SpeechRecog", event.getReplyCode() + " || " +
-                message.substring(0, Math.min(100, message.length())));
+    /**
+     * Receives the UID list of available Mails, asks for more data.
+     *
+     * @param message The raw message received
+     */
+    private void receiveMailEnumeration(String message){
+        String uidSeq = message.substring(9);
+        Log.d("IMAPListener/SpeechRecog", "--> " + uidSeq);
+        for (String uid : uidSeq.split("\\s")) {
+            try {
+                Log.d("IMAPListener/SpeechRecog", "UID " + uid);
+                Integer.parseInt(uid);
+                client.fetch(uid, "FLAGS");
+                client.fetch(uid, "BODY.PEEK[HEADER.FIELDS (Subject)]");
+                client.fetch(uid, "BODY.PEEK[HEADER.FIELDS (From)]");
+                client.fetch(uid, "BODY.PEEK[TEXT]");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NumberFormatException e) {
+            }
+        }
+    }
 
-        // Messages in folder
-        if (message.startsWith("* SEARCH")) {
-            String uidSeq = message.substring(9);
-            Log.d("IMAPListener/SpeechRecog", "--> " + uidSeq);
-            for (String uid : uidSeq.split("\\s")) {
-                try {
-                    Log.d("IMAPListener/SpeechRecog", "UID " + uid);
-                    Integer.parseInt(uid);
-                    client.fetch(uid, "FLAGS");
-                    client.fetch(uid, "BODY.PEEK[HEADER.FIELDS (Subject)]");
-                    client.fetch(uid, "BODY.PEEK[HEADER.FIELDS (From)]");
-                    client.fetch(uid, "BODY.PEEK[TEXT]");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (NumberFormatException e) {
-                }
+
+    private String extractFromField(String message){
+        try {
+            return MimeUtility.decodeText(message.split("\n")[1])
+                    .split(":", 2)[1];
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            try{
+                return message.split("\n")[1].split(":", 2)[1];
+            } catch (IndexOutOfBoundsException ioobe){
+                return message.split("\n")[1];
             }
-        } else if (message.contains(" FETCH ")) {
-            Log.d("IMAPClient/SpeechRecognizer", "?? " + message.split("\n")[0]);
-            if (message.split("\n")[0].toUpperCase().contains("FROM")) {
-                try {
-                    from = MimeUtility.decodeText(message.split("\n")[1])
-                            .split(":", 2)[1];
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (IndexOutOfBoundsException e) {
-                    from = "Sin remitente";
-                }
+        } catch (IndexOutOfBoundsException e) {
+            return "Sin remitente"; /// @TODO: Extract string
+        }
+    }
+
+
+    private String extractSubjectField(String message){
+        try {
+            return MimeUtility.decodeText(message.split("\n")[1])
+                    .split(":", 2)[1];
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            try{
+                return message.split("\n")[1].split(":", 2)[1];
+            } catch (IndexOutOfBoundsException ioobe){
+                return message.split("\n")[1];
             }
+        } catch (IndexOutOfBoundsException e) {
+            return "Sin asunto"; /// @TODO: Extract string
+        }
+    }
+
+
+    private boolean isFlagSeenSet(String message){
+        return message.split("\n")[0].toUpperCase().contains("SEEN");
+    }
+
+
+    private String extractMailBody(String message){
+        String[] lines = message.split("\n");
+        int l = lines.length - 1;
+        StringBuilder builder = new StringBuilder(l - 1);
+        for (int i = 1; i < l; i++) {
+            if (i > 1) {
+                builder.append("\n");
+            }
+            builder.append(lines[i]);
+        }
+        try {
+            return MimeUtility.decodeText(builder.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return builder.toString();
+        }
+    }
+
+
+    /**
+     * Receives an email field and stores it in the Mail object.
+     *
+     * @param message
+     */
+    private void receiveMailField(String message){
+        int uid = -1;
+        try {
+            uid = Integer.parseInt(message.split(" ")[1]);
+        } catch (IndexOutOfBoundsException e){
+            Log.e("IMAPClient/SpeechRecognizer", "Invalid FETCH line: " + message);
+            return;
+        } catch(NumberFormatException e){
+            Log.e("IMAPClient/SpeechRecognizer", "Invalid FETCH line, UID not found: " + message);
+            return;
+        }
+
+
+        if (message.split("\n")[0].toUpperCase().contains("FROM")) {
+            from = extractFromField(message);
         } else if (message.split("\n")[0].toUpperCase().contains("SUBJECT")) {
-            try {
-                subject = MimeUtility.decodeText(message.split("\n")[1])
-                        .split(":", 2)[1];
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (IndexOutOfBoundsException e) {
-                subject = "Sin asunto";
-            }
-
+            subject = extractSubjectField(message);
         } else if (message.split("\n")[0].toUpperCase().contains("TEXT")) {
-            String[] lines = message.split("\n");
-            int l = lines.length - 1;
-            StringBuilder builder = new StringBuilder(l - 1);
-            for (int i = 1; i < l; i++) {
-                if (i > 1) {
-                    builder.append("\n");
-                }
-                builder.append(lines[i]);
-            }
-            try {
-                text = MimeUtility.decodeText(builder.toString());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+            text = extractMailBody(message);
         } else if (message.split("\n")[0].toUpperCase().contains("FLAGS")) {
-            read = message.split("\n")[0].toUpperCase().contains("SEEN");
+            read = isFlagSeenSet(message);
         }
 
         /// @TODO use the UID to make sure the mail is assembled correctly
@@ -102,7 +148,21 @@ class IMAPListener implements ProtocolCommandListener {
             read = null;
             iface.addMail(mail);
         }
+    }
 
+
+    @Override
+    public void protocolReplyReceived(ProtocolCommandEvent event) {
+        String message = event.getMessage();
+        Log.d("IMAPListener/SpeechRecog", event.getReplyCode() + " || " +
+                message.substring(0, Math.min(100, message.length())));
+
+        // Messages in folder
+        if (message.startsWith("* SEARCH")) {
+            receiveMailEnumeration(message);
+        } else if (message.contains(" FETCH ")) {
+            receiveMailField(message);
+        }
 
     }
 
